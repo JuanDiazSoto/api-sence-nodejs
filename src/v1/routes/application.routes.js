f526964d2d18c39835ae0b59134e3f8e16d6fde9
Juan Díaz Soto <jfds.205@gmail.com>
2023-12-31 18:39:45 -0300

    Update application.routes.js

--- src/v1/routes/application.routes.js
+++ src/v1/routes/application.routes.js
@@ -5,5 +5,6 @@ const appController = require("../../controller/applicationController");
 router.get("/save",appController.createNewApp);
 router.get("/applications", appController.getApplications);
 router.get("/update", appController.updateApplication);
+router.get("/:id", appController.getApplication);
 
 module.exports = router;
