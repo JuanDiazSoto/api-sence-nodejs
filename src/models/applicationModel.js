e290b5986da97a51d11691c2c51a119d46de5871
Juan Díaz Soto <jfds.205@gmail.com>
2024-01-04 01:49:16 -0300

    Update applicationModel.js

--- src/models/applicationModel.js
+++ src/models/applicationModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const applicationSchema = new mongoose.Schema(
   {
     ip : { type : String , required : true},
-    idTrabajador : { type : string },
+    idTrabajador : { type : String },
     codSence: { type : String},
     codCurso: { type : String},
     IdSesionAlumno: { type : String},
