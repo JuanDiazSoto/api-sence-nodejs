225bfeb1193a5a6bfc40c055898db9ab9d4191e0
Juan Díaz Soto <jfds.205@gmail.com>
2024-01-13 21:10:15 -0300

    Create redirectController.js

---
+++ src/controller/redirectController.js
@@ -0,0 +1,26 @@
+const AppDB = require("../models/applicationModel");
+const dotenv = require("dotenv");
+const moment = require("moment");
+dotenv.config();
+
+
+const actualizaDB = async (req, res) => {
+
+    try {
+        const flag = true;
+        const id = req.body.IdSesionAlumno;
+        const IdSesionSense = req.body.IdSesionSense;
+        if(!req.body.GlosaError){
+            flag = false;
+        }
+        const href = req.query.href;
+
+        const filter = { GlosaError: glosaError, flag: flag };
+        const appUpdate = await AppDB.findByIdAndUpdate(id, filter, { new : true, });
+        res.redirect(href);
+
+    }catch(e){
+        console.log("Error Server : " , e);
+    }
+
+}
