ede866db1ac1e3781928f1b2597ab0ad982cb544
Juan Díaz Soto <jfds.205@gmail.com>
2024-01-15 22:03:33 -0300

    Update redirectController.js

--- src/controller/redirectController.js
+++ src/controller/redirectController.js
@@ -18,7 +18,7 @@ console.log("Redirect", req.body.IdSesionAlumno);
 
         const filter = { GlosaError: req.body.GlosaError, Flag: flag };
         const appUpdate = await AppDB.findByIdAndUpdate(id, filter, { new : true, });
-        const application = AppDB.find(id)
+        const application = AppDB.findById(id)
         console.log(application)
         res.redirect(application.href);
 
