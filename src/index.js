d96ce6bcaf430598f7b8d00b5e75be271faa1560
Juan Díaz Soto <jfds.205@gmail.com>
2024-02-06 14:16:27 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.get( '/', ( req, res ) => { res.send('Bienvenido api - logs - sence') });
 
 app.get('/macaddress', (req, res) => {
  // const  mac = getmac.default();
- const macAddress = first.first(); 
+ const macAddress = first.all(); 
  res.status(200).json(macAddress);
 });
 
