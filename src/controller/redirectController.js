0d5e222d86e5a05e9f1502f9fe966ecd3c7abdd4
Juan Díaz Soto <jfds.205@gmail.com>
2024-02-08 22:28:25 -0300

    Update redirectController.js

--- src/controller/redirectController.js
+++ src/controller/redirectController.js
@@ -4,32 +4,25 @@ const moment = require("moment");
 dotenv.config();
 
 
-const actualizaDB = async (req, res) => {
+const redirectIniciaSesion = async (req, res) => {
 
-    console.log("Redirect", req.body);
     try {
+        
         var flag = true;
         const id = req.body.IdSesionAlumno; 
         var IdSesionSense = "";
+        
         if(req.body.GlosaError != undefined){
             flag = false;
         }
         else{
          IdSesionSense  = req.body.IdSesionSence;
         }
-        console.log("PASO : idSesionSence", IdSesionSense);
-        console.log(id);
-        
-        //const href = req.query.href;
 
-        const filter = { GlosaError: req.body.GlosaError, Flag: flag , IdSesionSense : IdSesionSense };
-        console.log("filter", filter);
-        const appUpdate = await AppDB.findByIdAndUpdate(id, filter, { new : true, });
-        console.log("update", appUpdate);
-        
+        const dataUpdate = { GlosaError: req.body.GlosaError, Flag: flag , IdSesionSense : IdSesionSense };
+        const appUpdate = await AppDB.findByIdAndUpdate(id, dataUpdate, { new : true, });
         const application = await AppDB.findById(id);
-        console.log("update", appUpdate);
-        console.log(application)
+        
         res.redirect(decodeURIComponent(application.href));
 
     }catch(e){
@@ -67,5 +60,7 @@ const redirectCierraSesion = asyn (req, res) => {
 
 
 module.exports = {
-    actualizaDB
+    redirectIniciaSesion,
+    redirectCierraSesion 
+    
 };
