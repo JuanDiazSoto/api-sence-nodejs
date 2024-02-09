0e7db68844bdd032d1440a3eca229bc405471cf9
Juan Díaz Soto <jfds.205@gmail.com>
2024-02-09 09:28:17 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,8 +19,9 @@ app.use('/api/v1/logs', appRoutes);
 app.get( '/', ( req, res ) => { res.send('Bienvenido api - logs - sence') });
 
 app.post('/', redirectRoutes);
-//app.post('/redireccionar', redController.redirectIniciaSesion);
+//app.post('/redireccionar', redController.actualizaDB);
 app.post('/redirectInicioSesion', redController.redirectIniciaSesion);
 app.post('/redirectCierreSesion', redController.redirectCierraSesion);
+
 
 app.listen(PORT, () => { console.log("Server Listening") });
