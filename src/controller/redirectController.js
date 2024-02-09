ca2492ed3e5c42afddb3c8527ef1a66d61db7b7d
Juan Díaz Soto <jfds.205@gmail.com>
2024-02-08 22:26:39 -0300

    Update redirectController.js

--- src/controller/redirectController.js
+++ src/controller/redirectController.js
@@ -41,30 +41,28 @@ const redirectCierraSesion = asyn (req, res) => {
     console.log("Redirect Cierra Sesion : ", req);
 
     try{
-    }catch(e){
-
+        const id = req.body.IdSesionAlumno; 
+        const application = await AppDB.findById(id);
+        const url = application.href;
+        
         if(req.body.GlosaError != undefined){
-            
-           const id = req.body.IdSesionAlumno; 
-           const application = await AppDB.findById(id);
-           const url = application.href;
-
-            await AppDB.findByIdAndDelete(id, (error, success)=>{
-                if(error){
-                    console.log(`Ha ocurrido un error: ${error}`);                   
-                }else{
-                    console.log('Se ha Eliminado el registro');                    
-                }
-                res.redirect(decodeURIComponent(url));
-            })
-
-            
-            
+           
+          await AppDB.findByIdAndDelete(id, (error, success)=>{
+            if(error){
+                console.log(`Ha ocurrido un error: ${error}`);                   
+            }else{
+                console.log('Se ha Eliminado el registro');                    
+            }
+            res.redirect(decodeURIComponent(url));
+            });
+        }else{
+            const dataUpdate = { GlosaError: req.body.GlosaError };
+            const appUpdate = await AppDB.findByIdAndUpdate(id, dataUpdate, { new : true, });
+            res.redirect(decodeURIComponent(url));
         }
-            
-        
-    }
-        
+    }catch(e){
+        console.log("Error Server : " , e);
+    }        
 }
 
 
