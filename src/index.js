634593d6633a49621e33baa8ba96dd62caf87070
Juan Díaz Soto <jfds.205@gmail.com>
2024-03-11 16:05:39 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(cors());    
 
 app.use('/api/v1/logs', appRoutes);
-app.use('api/v1/util', utilRoutes );
+app.use('/api/v1/util', utilRoutes );
 app.get( '/', ( req, res ) => { res.send('Bienvenido api - logs - sence') });
 
 app.post('/', redirectRoutes);
